ddb9f7401ccfc357121697238a5163a5fa6e9436
吴雁江 <wuyjm@yonyou.com>
2017-03-03 14:36:25 +0800

    iOS

---
+++ src/ios/CitySelectPlugin.m
@@ -0,0 +1,58 @@
+/********* GXPlugin.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface CitySelectPlugin : CDVPlugin {
+    // Member variables go here.
+}
+- (void)startSelectCity:(CDVInvokedUrlCommand*)command;
+
+@end
+
+@implementation CitySelectPlugin
+    
+    
+- (void)pluginInitialize {
+   
+
+}
+
+- (void)startSelectCity:(CDVInvokedUrlCommand*)command{
+        
+        NSDictionary* paramters = [command.arguments objectAtIndex:0];
+        NSLog(@"传来的参数：%@",paramters);
+        
+        CityViewController *cityListVC = [[CityViewController alloc] init];
+        cityListVC.currentCityString = @"北京";
+        
+        [self presentViewController:cityListVC animated:true completion:^{
+            
+        }];
+        
+        [self.commandDelegate runInBackground:^{
+            
+            cityListVC.selectString = ^(NSString* citySting){
+                
+                NSLog(@"%@",citySting);
+                NSDictionary * dic = [NSDictionary dictionaryWithObject:citySting forKey:@"city"];
+                
+                CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:dic];
+                //            NSLog(@"sdfghgfdssdfghgfds%@,%@",commandResult,address);
+                
+                if (commandResult != NULL) {
+                    [self.commandDelegate sendPluginResult:commandResult callbackId:command.callbackId];
+                }
+                
+            };
+            
+        }];
+        
+        
+    }
+
+    
+    
+    
+    @end
+
+
---
+++ www/SSCCitySelect.js
@@ -0,0 +1,10 @@
+var exec = require('cordova/exec');
+
+var SSCCitySelect = {
+	start: function(parameters, success, error){
+		exec(success, error, "SSCCitySelect","startSelectCity",[parameters]);
+	}
+};
+
+module.exports = SSCCitySelect;
+
