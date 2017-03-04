08f569db4deab325fcefcfc74332cd851ac900a5
吴雁江 <wuyjm@yonyou.com>
2017-03-04 15:43:55 +0800

    3456789

--- src/ios/CitySelectPlugin.m
+++ src/ios/CitySelectPlugin.m
@@ -2,57 +2,76 @@
 
 #import <Cordova/CDV.h>
 #import "CityViewController.h"
+#import <MapKit/MapKit.h>
+#import "locationGPS.h"
+//#import "Myanotation.h"
+#import "LocationManager.h"
 @interface CitySelectPlugin : CDVPlugin {
     // Member variables go here.
 }
 - (void)startSelectCity:(CDVInvokedUrlCommand*)command;
-
+@property(nonatomic)locationGPS *loc;
 @end
 
 @implementation CitySelectPlugin
-    
-    
-- (void)pluginInitialize {
-   
 
+
+- (void)pluginInitialize {
+    
+    
 }
 
 - (void)startSelectCity:(CDVInvokedUrlCommand*)command{
+    
+    NSDictionary* paramters = [command.arguments objectAtIndex:0];
+    NSLog(@"传来的参数：%@",paramters);
+    
+    
+    self.loc = [locationGPS sharedlocationGPS];
+    [self.loc getAuthorization];//授权
+    [self.loc startLocation];//开始定位
+    self.loc.address = ^(NSString * address) {
         
-        NSDictionary* paramters = [command.arguments objectAtIndex:0];
-        NSLog(@"传来的参数：%@",paramters);
+        NSDictionary * dic = [NSDictionary dictionaryWithObject:address forKey:@"poiName"];
         
-        CityViewController *cityListVC = [[CityViewController alloc] init];
-        cityListVC.currentCityString = paramters[@"city"];
         
-        [self.viewController presentViewController:cityListVC animated:true completion:^{
+    };
+    
+    
+    
+    
+    
+    CityViewController *cityListVC = [[CityViewController alloc] init];
+    cityListVC.currentCityString = paramters[@"city"];
+    
+    [self.viewController presentViewController:cityListVC animated:true completion:^{
+        
+    }];
+    
+    [self.commandDelegate runInBackground:^{
+        
+        cityListVC.selectString = ^(NSString* citySting){
             
-        }];
-        
-        [self.commandDelegate runInBackground:^{
+            NSLog(@"%@",citySting);
+            NSDictionary * dic = [NSDictionary dictionaryWithObject:citySting forKey:@"city"];
             
-            cityListVC.selectString = ^(NSString* citySting){
-                
-                NSLog(@"%@",citySting);
-                NSDictionary * dic = [NSDictionary dictionaryWithObject:citySting forKey:@"city"];
-                
-                CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:dic];
-                //            NSLog(@"sdfghgfdssdfghgfds%@,%@",commandResult,address);
-                
-                if (commandResult != NULL) {
-                    [self.commandDelegate sendPluginResult:commandResult callbackId:command.callbackId];
-                }
-                
-            };
+            CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:dic];
+            //            NSLog(@"sdfghgfdssdfghgfds%@,%@",commandResult,address);
             
-        }];
+            if (commandResult != NULL) {
+                [self.commandDelegate sendPluginResult:commandResult callbackId:command.callbackId];
+            }
+            
+        };
         
-        
-    }
+    }];
+    
+    
+}
 
-    
-    
-    
-    @end
+
+
+
+@end
 
 
